44ed2de51dcdd48c0d981d9cba3f167711ce336a
adscud <arthurdelbeke@smartness.co>
2022-03-06 23:32:45 +0100

    scale heart icon

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Button, View} from 'react-native';
+import Animated, {
+    interpolate,
+    useAnimatedStyle,
+    useSharedValue,
+    withSequence,
+    withTiming
+} from 'react-native-reanimated';
+import {FontAwesome} from '@expo/vector-icons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const scaleLike = useSharedValue(1)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const likeStyle = useAnimatedStyle(() => {
+        return {
+            zIndex: 50,
+            opacity: interpolate(scaleLike.value, [1, 2], [0, 1, 0]),
+            transform: [
+                {scale: scaleLike.value},
+            ]
+        };
+    }, []);
+
+    const onPress = () => {
+        scaleLike.value = withSequence(
+            withTiming(2),
+            withTiming(1)
+        );
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: 'pink',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Animated.View
+                style={likeStyle}
+            >
+                <FontAwesome name="heart" size={100} color="red"/>
+            </Animated.View>
+            <Button title={'Like'} onPress={onPress}/>
+        </View>
+    );
+}
